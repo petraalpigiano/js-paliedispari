9f481e4d83a4d58664b8d71a019008ac1f6849a4
petraalpigiano <petraalpigiano@gmail.com>
2025-04-07 18:07:51 +0200

    add function

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -1,19 +1,27 @@
 // RACCOLTA DATI
 const userWord = prompt("Inserisci una parola", "cipolla");
 let wordReverse = "";
-for (let i = 0; i < userWord.length; i++) {
-  const currentCharacter = userWord[i];
-  wordReverse = currentCharacter + wordReverse;
-}
-
-if (userWord === wordReverse) {
-  console.log(wordReverse, ":", "La parola è palindroma");
-  alert("La parola è palindroma");
-} else {
-  console.log(wordReverse, ":", "La parola non è palindroma");
-  alert("La parola non è palindroma");
-}
 
 // SVOLGIMENTO
-// function isPalindrome(params) {}
+/**
+ *
+ * @param {string} userWord parola inserita dall'utente
+ * @param {string} wordReverse parola invertita
+ * @returns {boolean} indica se la parola è palindroma
+ */
+function isPalindrome(userWord, wordReverse) {
+  for (let i = 0; i < userWord.length; i++) {
+    const currentCharacter = userWord[i];
+    wordReverse = currentCharacter + wordReverse;
+  }
+
+  if (userWord === wordReverse) {
+    alert("La parola è palindroma");
+    return true;
+  } else {
+    alert("La parola non è palindroma");
+    return false;
+  }
+}
 // OUTPUT
+console.log("La parola è palindroma:", isPalindrome(userWord, wordReverse));
