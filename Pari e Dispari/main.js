00318918c20df996bbdceb2458ce303e39e73271
petraalpigiano <petraalpigiano@gmail.com>
2025-04-07 18:57:47 +0200

    add 1°function

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -3,18 +3,27 @@ const userEvenOrOdd = prompt("Scegli pari o dispari", "pari");
 const userNumber = parseInt(prompt("Scegli un numero da 1 a 5", "3"));
 console.log("utente pari o dispari:", userEvenOrOdd);
 console.log("utente numero tra 1 e 5:", userNumber);
-const pcNumber = Math.floor(Math.random() * 5) + 1;
-console.log("numero pc tra 1 e 5:", pcNumber);
-const sumPcUserNumber = userNumber + pcNumber;
+
+/**
+ *
+ * @param {number} max
+ * @param {number} min
+ * @returns {number}
+ */
+function generateRandomNumber(max, min = 1) {
+  const pcNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return pcNumber;
+}
+console.log("numero pc tra 1 e 5:", generateRandomNumber(5));
+
+const sumPcUserNumber = userNumber + generateRandomNumber(5);
 console.log("somma numeri:", sumPcUserNumber);
 
 // SVOLGIMENTO
 if (sumPcUserNumber % 2 === 0 && userEvenOrOdd === "pari") {
-  //pari
   console.log("Utente ha vinto");
   alert("Utente ha vinto");
 } else if (sumPcUserNumber % 2 === 1 && userEvenOrOdd === "dispari") {
-  //dispari
   console.log("Utente ha vinto");
   alert("Utente ha vinto");
 } else {
